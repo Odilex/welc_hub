386872fcc0a03a3a127d15dcda1f64b5da8412f3
odilex <didierjnuior75@gmail.comm>
2024-10-11 11:34:54 +0200

    nothing

--- welcomehub-frontend/src/App.js
+++ welcomehub-frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
 
-function App() {
+function Navbar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </ul>
+    </nav>
   );
 }
 
-export default App;
+export default Navbar;
