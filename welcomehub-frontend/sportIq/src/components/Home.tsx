28f172b5f57683cefea4d7af53091e614b2a7c95
odilex <didierjunior75@gmail.com>
2024-10-21 01:19:42 +0200

    frontent environment totaly changed

---
+++ welcomehub-frontend/sportIq/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Counter from './Counter';
+
+const Home: React.FC = () => (
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <Counter />
+  </div>
+);
+
+export default Home;
--- welcomehub-frontend/src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
